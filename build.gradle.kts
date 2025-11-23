74ff690d44110201c4ed2dc8025d1c35baee8e85
Teahyung Koo <150074724+koosco@users.noreply.github.com>
2025-11-24 02:53:48 +0900

    Fix/Add Component annotation to usecase (#3)
    * add Component annotation to UseCase annotation for DI

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.koosco"
-version = "0.0.2"
+version = "0.0.3"
 description = "Common core library"
 
 java {
--- src/main/kotlin/com/koosco/common/core/annotation/UseCase.kt
+++ src/main/kotlin/com/koosco/common/core/annotation/UseCase.kt
@@ -1,5 +1,8 @@
 package com.koosco.common.core.annotation
 
+import org.springframework.stereotype.Component
+
+@Component
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class UseCase()
